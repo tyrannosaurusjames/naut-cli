e644c1be8f7f2368bd809e0307b7943bbd7a6901
James Goodman <james.s.goodman@gmail.com>
2017-07-07 20:29:14 +1200

    Update deploy and log service

--- src/Service/DeployLogService.php
+++ src/Service/DeployLogService.php
@@ -11,21 +11,28 @@ class DeployLogService
         $output = '';
         $status = 'Queued';
 
-        while ($status === 'Queued' || $status === 'Running') {
+        $runningStatuses = [
+            'New',
+            'Approved',
+            'Queued',
+            'Deploying'
+        ];
+
+        while (in_array($status, $runningStatuses)) {
             $lastOutput = $output;
 
             $response = $client->get($statusLink);
             $responseData = json_decode($response->getBody()->getContents(), true);
 
-            $status = $responseData['status'];
-            $output = $responseData['message'];
+            $status = $responseData['data']['attributes']['state'];
+            $output = implode(PHP_EOL, $responseData['message']);
             $printableOutput = str_replace($lastOutput, '', $output);
 
             echo $printableOutput;
             sleep(1);
         }
 
-        return ($status === 'Complete');
+        return ($status === 'Completed');
     }
 
 }
