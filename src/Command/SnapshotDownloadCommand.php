d048b326f4a7107ca3b0925c9fc33885766798bb
James Goodman <james.goodman@careers.govt.nz>
2017-11-24 15:34:13 +1300

    Add destination option to download command

--- src/Command/SnapshotDownloadCommand.php
+++ src/Command/SnapshotDownloadCommand.php
@@ -6,6 +6,7 @@ use Guttmann\NautCli\Service\SnapshotDownloadService;
 use Guttmann\NautCli\Service\SnapshotService;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SnapshotDownloadCommand extends ContainerAwareCommand
@@ -17,13 +18,16 @@ class SnapshotDownloadCommand extends ContainerAwareCommand
             ->setDescription('Download a snapshot')
             ->setHelp('Given a stack and a snapshot id this command will download a snapshot')
             ->addArgument('stack', InputArgument::REQUIRED, 'The id of your stack')
-            ->addArgument('snapshot_id', InputArgument::REQUIRED, 'The id of the snapshot');
+            ->addArgument('snapshot_id', InputArgument::REQUIRED, 'The id of the snapshot')
+            ->addOption('destination', null, InputOption::VALUE_REQUIRED, 'File name and path to store download');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $stack = $input->getArgument('stack');
         $snapshotId = $input->getArgument('snapshot_id');
+
+        $destination = $input->getOption('destination');
 
         $container = $this->getContainer();
 
